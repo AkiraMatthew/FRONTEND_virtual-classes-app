d5a5258c9fc19cd4d0ccc3a148cf269088cc4f26
AkiraMatthew <akira.dev.math@gmail.com>
2023-04-12 01:02:15 -0300

    section implementation

--- src/components/homeNoAuth/presentationSection/index.tsx
+++ src/components/homeNoAuth/presentationSection/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from './styles.module.scss';
 import { Container, Row, Col, Button } from 'reactstrap';
 
@@ -7,19 +8,23 @@ const PresentationSection = function () {
             <Container>
                 <Row>
                     <Col md>
-                        <p>UNLIMITED ACCESS</p>
-                        <p>
+                        <p className={styles.subTitle}>UNLIMITED ACCESS</p>
+                        <p className={styles.title}>
                             Have access to the best <br /> software development tutorials.
                         </p>
-                        <p>
+                        <p className={styles.description}>
                             Study anywhere and anytime, and keep <br />
                             evolving as developer.
                         </p>
-                        <Button>
-                            ACCESS NOW <img src="/imgs/buttonPlay.svg" alt="button_img" />
-                        </Button>
+                        <Link href="/register">
+                            <Button outline className={styles.btnCta}>
+                                ACCESS NOW <img src="/imgs/buttonPlay.svg" alt="button_img" className={styles.btnImg} />
+                            </Button>
+                        </Link>
                     </Col>
-                    <Col md></Col>
+                    <Col md>
+                        <img src="/imgs/homeNoAuth/imgPresentation.png" alt="imgIndex" className={styles.imgPresentation} />
+                    </Col>
                 </Row>
             </Container>
         </>
