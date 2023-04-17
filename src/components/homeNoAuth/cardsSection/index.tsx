7a4a7deac1adf9f23ecc59c8ad3c4ee1906d860a
AkiraMatthew <akira.dev.math@gmail.com>
2023-04-17 13:23:36 -0300

    heigh fix

---
+++ src/components/homeNoAuth/cardsSection/index.tsx
@@ -0,0 +1,21 @@
+import { Container } from 'reactstrap';
+import styles from './styles.module.scss';
+
+const CardsSection = function () {
+    return (
+        <>
+            <p>WHAT YOU WANT TO ACCESS</p>
+            <Container>
+                <div>
+                    <p>FRONT-END</p>
+                    <p>
+                        Onebitcode Black is the place for you to evolve. For this, you will have access to advanced programming
+                        practices, technology updates, and all the technical support you need to become a programming senior.
+                    </p>
+                </div>
+            </Container>
+        </>
+    );
+};
+
+export default CardsSection;
