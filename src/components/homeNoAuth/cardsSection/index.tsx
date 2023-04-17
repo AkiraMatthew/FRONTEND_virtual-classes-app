695b7b319b419213397687eef35761defef5ba35
AkiraMatthew <akira.dev.math@gmail.com>
2023-04-17 16:45:22 -0300

    creating visual elements, responsivity N cards, homeNoAuth

--- src/components/homeNoAuth/cardsSection/index.tsx
+++ src/components/homeNoAuth/cardsSection/index.tsx
@@ -4,11 +4,46 @@ import styles from './styles.module.scss';
 const CardsSection = function () {
     return (
         <>
-            <p>WHAT YOU WANT TO ACCESS</p>
-            <Container>
-                <div>
-                    <p>FRONT-END</p>
-                    <p>
+            <p className={styles.sectionTitle}>WHAT DO YOU WANT TO ACCESS?</p>
+            <Container className='d-flex flex-wrap justify-content-center gap-4 pb-5'>
+                <div className={styles.card1}>
+                    <p className={styles.cardTitle}>FRONT-END</p>
+                    <p className={styles.cardDescription}>
+                        Onebitcode Black is the place for you to evolve. For this, you will have access to advanced programming
+                        practices, technology updates, and all the technical support you need to become a programming senior.
+                    </p>
+                </div>
+                <div className={styles.card2}>
+                    <p className={styles.cardTitle}>BACK-END</p>
+                    <p className={styles.cardDescription}>
+                        Onebitcode Black is the place for you to evolve. For this, you will have access to advanced programming
+                        practices, technology updates, and all the technical support you need to become a programming senior.
+                    </p>
+                </div>
+                <div className={styles.card3}>
+                    <p className={styles.cardTitle}>MOBILE</p>
+                    <p className={styles.cardDescription}>
+                        Onebitcode Black is the place for you to evolve. For this, you will have access to advanced programming
+                        practices, technology updates, and all the technical support you need to become a programming senior.
+                    </p>
+                </div>
+                <div className={styles.card4}>
+                    <p className={styles.cardTitle}>GIT AND GITHUB</p>
+                    <p className={styles.cardDescription}>
+                        Onebitcode Black is the place for you to evolve. For this, you will have access to advanced programming
+                        practices, technology updates, and all the technical support you need to become a programming senior.
+                    </p>
+                </div>
+                <div className={styles.card5}>
+                    <p className={styles.cardTitle}>PROJECTS</p>
+                    <p className={styles.cardDescription}>
+                        Onebitcode Black is the place for you to evolve. For this, you will have access to advanced programming
+                        practices, technology updates, and all the technical support you need to become a programming senior.
+                    </p>
+                </div>
+                <div className={styles.card6}>
+                    <p className={styles.cardTitle}>CAREERS</p>
+                    <p className={styles.cardDescription}>
                         Onebitcode Black is the place for you to evolve. For this, you will have access to advanced programming
                         practices, technology updates, and all the technical support you need to become a programming senior.
                     </p>
