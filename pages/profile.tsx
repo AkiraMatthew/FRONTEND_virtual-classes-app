0a73f8bf07adc9c9fa57b373b2bcef106df05881
AkiraMatthew <akira.dev.math@gmail.com>
2023-05-05 18:51:05 -0300

    user profile style and build

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,6 +1,9 @@
 import Head from 'next/head';
 import styles from '../styles/profile.module.scss';
 import UserForm from '@/components/profile/user';
+import HeaderAuth from '@/components/common/headerAuth';
+import Footer from '@/components/common/footer';
+import { Button, Col, Container, Row } from 'reactstrap';
 
 const UserInfo = function () {
     return (
@@ -10,7 +13,19 @@ const UserInfo = function () {
                 <link rel="icon" href="/imgs/favicon.svg" type="image/x-icon" />
             </Head>
             <main>
-                <UserForm />
+                <HeaderAuth />
+                <Container className="py-5">
+                    <p className={styles.title}>My account</p>
+                    <Row className='pt-3 pb-5'>
+                        {/* The bootstrap grid is divided in 12, in the following md col size, it will have the 4 size */}
+                        <Col md={4} className={styles.btnColumn}>
+                            <Button className={styles.renderForm}>Personal Data</Button>
+                            <Button className={styles.renderForm}>Password</Button>
+                        </Col>
+                        <Col md><UserForm /></Col>
+                    </Row>
+                </Container>
+                <Footer />
             </main>
         </>
     );
--- src/components/profile/user/index.tsx
+++ src/components/profile/user/index.tsx
@@ -1,18 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
-import { Form } from 'reactstrap';
+import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
 import styles from '../../../../styles/profile.module.scss';
 
 const UserForm = function () {
     return (
         <>
-            <Form>
+            <Form className={styles.form}>
                 {/* First paragraph is the person name abreviation */}
                 <div className={styles.formName}>
                     <p className={styles.nameAbbreviation}>NT</p>
                     <p className={styles.userName}>NAME TEST</p>
                 </div>
                 {/* Here we put the information of the user membership time */}
-                <div className={styles.formName}>
+                <div className={styles.memberTime}>
                     <img
                         src="/imgs/profile/iconUserAccount.svg"
                         alt="iconProfile"
@@ -21,6 +21,65 @@ const UserForm = function () {
                     <p className={styles.memberTimeText}>
                         Member since <br /> 20st April 2020
                     </p>
+                </div>
+
+                <hr />
+
+                <div>
+                    <FormGroup>
+                        <Label for="firstName" className={styles.label}>NAME</Label>
+                        <Input
+                            name="firstName"
+                            type="text"
+                            id="firstName"
+                            placeholder="What is your first name"
+                            required
+                            maxLength={20}
+                            className={styles.inputFlex}
+                            value={'Name'}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="lastName" className={styles.label}>LAST NAME</Label>
+                        <Input
+                            name="lastName"
+                            type="text"
+                            id="lastName"
+                            placeholder="What is your last name"
+                            required
+                            maxLength={20}
+                            className={styles.inputFlex}
+                            value={'Test'}
+                        />
+                    </FormGroup>
+                </div>
+                <div>
+                    <FormGroup>
+                        <Label for="number" className={styles.label}>PHONE NUMBER</Label>
+                        <Input
+                            name="phone"
+                            type="text"
+                            id="phone"
+                            placeholder="+xx (xx) xxxx-xxxx"
+                            required
+                            className={styles.input}
+                            value={'+55 21 98765-4321'}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email" className={styles.label}>E-MAIL</Label>
+                        <Input
+                            name="email"
+                            type="email"
+                            id="email"
+                            placeholder="dunno@email.com"
+                            required
+                            className={styles.input}
+                            value={'dunno@email.com'}
+                        />
+                    </FormGroup>
+
+                    <Button className={styles.formBtn} outline type='submit'></Button>
                 </div>
             </Form>
         </>
