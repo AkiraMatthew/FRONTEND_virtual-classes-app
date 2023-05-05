9eaaf5992fe435f3d197d4071d602198262e6090
AkiraMatthew <akira.dev.math@gmail.com>
2023-05-05 01:10:52 -0300

    favorites section elements creation

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,4 +1,5 @@
 import HeaderAuth from '@/components/common/headerAuth';
+import FavoriteCategory from '@/components/homeAuth/favoriteCategory';
 import FeaturedSection from '@/components/homeAuth/featuredSection';
 import NewestCategory from '@/components/homeAuth/newestCategory';
 import Head from 'next/head';
@@ -14,6 +15,7 @@ const HomeAuth = function () {
                 {/* <HeaderAuth /> */}
                 <FeaturedSection />
                 <NewestCategory />
+                <FavoriteCategory />
             </main>
         </>
     );
--- src/components/homeAuth/favoriteCategory/index.tsx
+++ src/components/homeAuth/favoriteCategory/index.tsx
@@ -1,7 +1,33 @@
-import styles from './styles.module.scss';
+import useSWR from 'swr';
+import styles from '../../../../styles/slideCategory.module.scss';
+import courseService from '@/services/courseService';
+import SlideComponent from '@/components/common/slideComponent';
 
 const FavoriteCategory = function () {
-    return <></>;
+    //creating the SWR use
+    const { data, error } = useSWR('/favorites', courseService.getFavCourses);
+
+    if (error) return error;
+    if (!data)
+        return (
+            <>
+                <p>Loading...</p>
+            </>
+        );
+
+    return (
+        <>
+            <p className={styles.titleCategory}>My List</p>
+            {/* there is 2 states on the course favorites, if a favorite exists or if it does not exist */}
+            {data.data.courses.length >= 1 ? (
+                <SlideComponent course={data.data.courses} />
+            ) : (
+                <p className='text-center pt-3 h5'>
+                    <strong>You still have no courses on the list</strong>
+                </p>
+            )}
+        </>
+    );
 };
 
 export default FavoriteCategory;
